27d5e52674d3e9f6f666f304731024c36328cf51
mel-j-vickery <u157233473@byui.edu>
2024-06-06 14:59:19 -0600

    starting C# inclass work

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,11 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("I am learning how to use C#. YAY!!");
     }
 }
