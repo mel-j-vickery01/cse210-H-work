eb4470e5068817740095c9e2228e78ca3ed0f65a
mel-j-vickery <u157233473@byui.edu>
2024-06-12 14:58:44 -0600

    in class work on C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter you grade percentige: ");
+        string percent_str = Console.ReadLine();
+        int calc_int = int.Parse(percent_str);
+        Console.Write("Your grade is ");
+        if (calc_int >= 90)
+        {
+            Console.Write("A");
+        }
+        else if (calc_int >= 80)
+        {
+            Console.Write("B");
+        }
+        else if (calc_int >= 70)
+        {
+            Console.Write("C");
+        }
+        else if (calc_int >= 60)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("F");
+        }
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Runtime.InteropServices;
+// using System.Runtime.InteropServices;
 
 
 class Program
@@ -7,11 +7,56 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("I am learning how to use C#. YAY!!");
-    
-for (int i = 0 ; i < 10 ; i++)
-{
-    Console.WriteLine( "bob\n" + i);
-}
+// int userAge = 0;
+
+// while (userAge < 0 || userAge > 120)
+// {
+//     Console.Write("Please input your age (0-120): ");
+//     userAge = int.Parse(Console.ReadLine());
+// }
+
+// int userAge2 = 0;
+// do
+// {
+//     Console.Write("Please enter your age (0-120)");
+//     userAge = int.Parse(Console.ReadLine());
+// }while (userAge2 < 0 || userAge2 > 120);
+
+// for(int i = -10000000; i < 1000000000; i += 10000)
+// {
+//     Console.WriteLine("" + i);
+// }
+
+// List<int> myNumbers = new List<int>();
+// List<string> myNames = new List<string>();
+
+// myNames.Add("Bob");
+// myNames.Add("Mel");
+// myNames.Add("Trevor");
+// myNames.Add("Emily");
+// myNames.Add("Noah");
+
+// myNumbers.Add(42);
+// myNumbers.Add(78);
+// myNumbers.Add(68);
+// myNumbers.Add(98);
+// myNumbers.Add(20);
+
+// foreach(string name in myNames)
+// {
+//     Console.WriteLine("" + name);
+// }
+// foreach(int number in myNumbers)
+// {
+//     Console.WriteLine("" + number);
+// }
+static double CalculateCircleArea(double radius)
+    {
+        double area = 0;
+        area = Math.PI * radius * radius;
+        return area;
     }
+    double area = CalculateCircleArea(10);
+    Console.Write(area);
 }
+
