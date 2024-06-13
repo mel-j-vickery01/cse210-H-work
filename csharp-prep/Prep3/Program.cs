6867bf949ec9218cd9c5caf9182163e6f7a778d0
mel-j-vickery <u157233473@byui.edu>
2024-06-13 12:04:06 -0600

    work on prep3 added while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the Magic Number? :");
+        string userSetGuess = Console.ReadLine();
+        int magicNumber = int.Parse(userSetGuess);
+
+
+        string answer = ("yes");
+
+        while (answer == "yes")
+        {
+                Console.Write("What is your guess? :");
+        string userInputGuess = Console.ReadLine();
+        int userGuess = int.Parse(userInputGuess);
+
+
+        if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Guess lower");
+        }
+        if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Guess Higher");
+        }
+        if (userGuess == magicNumber)
+        {
+            Console.WriteLine("You Guessed the Magic Number!!!");
+            answer = "no";
+        }
+        }
     }
 }
