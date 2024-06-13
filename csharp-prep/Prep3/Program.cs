8602b8e6fb89ccb6792407d2ba3bc84b45aed330
mel-j-vickery01 <u157233473@byui.edu>
2024-06-13 11:34:28 -0600

    work on prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the Magic Number? :");
+        string userSetGuess = Console.ReadLine();
+        int magicNumber = int.pars(userSetGuess);
+
+        Console.Write("What is your guess? :");
+        string userInputGuess = Console.ReadLine();
+        int userGuess = int.pars(userInputGuess);
+
+        if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Guess lower")
+        }
+
     }
 }
