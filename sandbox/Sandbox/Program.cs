e84f1f1b538150109d8b65fcf27cfbc694067746
mel-j-vickery <u157233473@byui.edu>
2024-06-17 14:55:23 -0600

    In-class. work on objects

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,60 +7,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
-// int userAge = 0;
 
-// while (userAge < 0 || userAge > 120)
-// {
-//     Console.Write("Please input your age (0-120): ");
-//     userAge = int.Parse(Console.ReadLine());
-// }
+        Circle myCircle = new Circle(10);
+        Console.WriteLine($"{myCircle.GetArea()}");
 
-// int userAge2 = 0;
-// do
-// {
-//     Console.Write("Please enter your age (0-120)");
-//     userAge = int.Parse(Console.ReadLine());
-// }while (userAge2 < 0 || userAge2 > 120);
-
-// for(int i = -10000000; i < 1000000000; i += 10000)
-// {
-//     Console.WriteLine("" + i);
-// }
-
-// List<int> myNumbers = new List<int>();
-// List<string> myNames = new List<string>();
-
-// myNames.Add("Bob");
-// myNames.Add("Mel");
-// myNames.Add("Trevor");
-// myNames.Add("Emily");
-// myNames.Add("Noah");
-
-// myNumbers.Add(42);
-// myNumbers.Add(78);
-// myNumbers.Add(68);
-// myNumbers.Add(98);
-// myNumbers.Add(20);
-
-// foreach(string name in myNames)
-// {
-//     Console.WriteLine("" + name);
-// }
-// foreach(int number in myNumbers)
-// {
-//     Console.WriteLine("" + number);
-// }
-static double CalculateCircleArea(double radius)
-    {
-        double area = 0;
-        area = Math.PI * radius * radius;
-        return area;
+        Circle unitCircle = new Circle(10);
+        Console.WriteLine($"{unitCircle.GetDiametar()}");
+        Console.WriteLine($"{unitCircle.GetRadius()}");
     }
-    double area = CalculateCircleArea(10);
-    Console.WriteLine($"F3:the circle area is: {area.ToString("F3")}");
-
-    int x = 65535;
-    Console.WriteLine($"N: The circle's area is: {x.ToString("N")}");
-}
 }
 
---
+++ sandbox/Sandbox/june 17.cs
@@ -0,0 +1,32 @@
+class Circle
+{
+    private double _radius;
+
+    public Circle(double radius = 0.0)
+    {
+        _radius = radius;
+
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+
+    }
+
+    public double GetDiametar()
+    {
+        return _radius * 2;
+    }
+    
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+}
