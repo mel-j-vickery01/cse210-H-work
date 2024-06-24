8b7a4ed776712a801bc57da0008ee77310ce5dda
mel-j-vickery <u157233473@byui.edu>
2024-06-24 14:59:19 -0600

    in-class work on constructors

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,28 @@
+class Fraction
+{
+private int _top;
+private int _bottom;
+
+public Fraction()
+{
+    _top = 1;
+    _bottom = 1;
+    Console.WriteLine($"{_top} / {_bottom}");
+}
+public Fraction(int top)
+{
+    _top = top;
+    _bottom = 1;
+    Console.WriteLine($"{_top} / {_bottom}");
+}
+public Fraction(int top, int bottom)
+{
+    _top = top;
+    _bottom = bottom;
+    Console.WriteLine($"{_top} / {_bottom}");
+
+}
+
+
+
+}
